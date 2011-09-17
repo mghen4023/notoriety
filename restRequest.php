d588e912672e130f3e1341bcd3b9ebe7ae9fbd0c
Max DeLiso <maxdeliso@gmail.com>
2011-09-17 10:27:43 -0400

    flushed out the server side RESTful twitter api calls. started to implement client side streaming api calls.

--- restRequest.php
+++ restRequest.php
@@ -14,13 +14,61 @@ $requestConnection = new TwitterOAuth(
      $_SESSION['tokenCredentials']['oauth_token_secret']);
 
 $content = $requestConnection->get('account/verify_credentials');
-
 if( isset( $content ) )
 {
+	echo('<hr><br><b> verify_credentials </b><br>');
 	var_dump($content);
 } else {
 	echo('an error occurred: content variable was not set');
-	echo '<br><hr>';
+	exit(1);
 }
 
+$content = $requestConnection->get('account/rate_limit_status');
+if( isset( $content ) )
+{
+	echo('<hr><br><b> rate limit status </b><br>');
+	var_dump($content);
+} else {
+	echo('an error occurred: content variable was not set');
+	exit(1);
+}
+
+$content = $requestConnection->get('geo/search', array('ip' => $_SERVER['REMOTE_ADDR']) );
+if( isset( $content ) )
+{
+	echo('<hr><br><b> ip-based geo lookup </b><br><hr>');
+	var_dump($content);
+} else {
+	echo('an error occurred: content variable was not set');
+	exit(1);
+}
+
+echo '<hr><br><b> for js: </b><br><hr>';
+
+
+
+</script>
+
+
 ?>
+
+<!-- 
+<span id="login"></span>
+<script type="text/javascript">
+
+
+twttr.anywhere(function (T) {
+	T("#login").connectButton({
+		authComplete: function(user) {
+			// triggered when auth completed successfully
+		},
+		signOut: function() {
+			// triggered when user logs out
+		}
+	});
+});
+
+
+
+</script>
+ -->
