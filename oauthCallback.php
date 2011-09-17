d5199ab6d8277be99f305efa4d2b6dd0a43a82fb
Max DeLiso <maxdeliso@gmail.com>
2011-09-17 12:54:20 -0400

    consolidated and renamed the server side oauth code flow

--- oauthCallback.php
+++ oauthCallback.php
@@ -1,7 +1,6 @@
 <?php
-
 /*
- * This file gets the access token for the application
+ * This file gets the access token for the application and uses it to sign a buch of RESTful API requests.
  */
 include_once 'includes/twitteroauth.php';
 
@@ -19,26 +18,18 @@ $credentialsConnection = new TwitterOAuth(
 
 $tokenCredentials = $credentialsConnection->getAccessToken($_REQUEST['oauth_verifier']);
 
-$_SESSION['tokenCredentials'] = $tokenCredentials;      
-
-/* may not work!
- 
-  
 $requestConnection = new TwitterOAuth(
      'ward0Wz6G6I7UlbSSeZiQ', 
      'IYqEHAekTYi53yp9wsaWVpTT1YS56UUUsvafZtU',
-$_SESSION['tokenCredentials']['oauth_token'],
-$_SESSION['tokenCredentials']['oauth_token_secret']);
+$tokenCredentials['oauth_token'],
+$tokenCredentials['oauth_token_secret']);
 
-echo 'TOKEN: ' . $_SESSION['tokenCredentials']['oauth_token'] . '<br/>'; 
-echo 'SECRET: ' . $_SESSION['tokenCredentials']['oauth_token_secret'] . '<br/>';
+echo 'TOKEN: ' . $tokenCredentials['oauth_token'] . '<br/>'; 
+echo 'SECRET: ' . $tokenCredentials['oauth_token_secret'] . '<br/>';
 
 $content = $requestConnection->get('account/verify_credentials');
 if( isset( $content ) )
 {
-	//echo('<hr><br><b> verify_credentials </b><br>');
-	//var_dump($content);
-	//GOES IN verify_credentials
 	echo 'NAME: ';
 	echo $content->name . '<br/>';
 	echo 'SCREEN NAME: ';
@@ -54,9 +45,6 @@ if( isset( $content ) )
 $content = $requestConnection->get('account/rate_limit_status');
 if( isset( $content ) )
 {
-	//echo('<hr><br><b> rate limit status </b><br>');
-	//var_dump($content);
-	//GOES IN rate limit status
 	echo 'RATE LIMIT (REMAINING HITS): ';
 	echo $content->remaining_hits . '<br />';
 
@@ -68,10 +56,6 @@ if( isset( $content ) )
 $content = $requestConnection->get('geo/search', array('ip' => $_SERVER['REMOTE_ADDR']) );
 if( isset( $content ) )
 {
-	//echo('<hr><br><b> ip-based geo lookup </b><br><hr>');
-	//var_dump($content);
-
-	//GOES IN ip-based geo lookup
 	echo 'COUNTRY CODE: ';
 	echo $content->result->places[0]->country_code . '<br />';
 	echo 'CITY: ';
@@ -83,12 +67,4 @@ if( isset( $content ) )
 	exit(1);
 }
 
-//echo '<hr><br><b> for js: </b><br><hr>';
-
-
- 
-  
- */
-
-echo '<a href="' . 'restRequest.php' . '"> do request! </a>';
 ?>
--- oauthStart.php
+++ oauthStart.php
@@ -12,12 +12,12 @@ $appTokenConnection = new TwitterOAuth(
     'ward0Wz6G6I7UlbSSeZiQ', 
     'IYqEHAekTYi53yp9wsaWVpTT1YS56UUUsvafZtU');
 $appTempCredentials = $appTokenConnection->getRequestToken(
-    'http://www.sanguineshuriken.com/oauthCont.php');
+    'http://www.sanguineshuriken.com/oauthCallback.php');
 
 $_SESSION['application_oauth_token'] = $appTempCredentials['oauth_token'];
 $_SESSION['application_oauth_token_secret'] = $appTempCredentials['oauth_token_secret'];
 
-echo 'app temp credentials are: ';
+echo '(DEBUG) app temp credentials are: ';
 print_r($appTempCredentials);
 echo '<br><hr>';
 
@@ -25,5 +25,3 @@ $registerURL = $appTokenConnection->getAuthorizeURL($appTempCredentials, TRUE);
 
 echo '<a href="' . $registerURL . '">Auth with Twitter</a>';
 ?>
-
-
