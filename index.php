b481b7065faea4e3219fb2962dba902557a106cb
Chris <cts164@psu.edu>
2011-09-17 04:09:26 -0400

    Extracted Content to iFrame

--- index.php
+++ index.php
@@ -1,18 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
 <html> 
 	<head> 
-		<script src="http://platform.twitter.com/anywhere.js?id=ward0Wz6G6I7UlbSSeZiQ&v=1" type="text/javascript">
+		<!--script src="http://platform.twitter.com/anywhere.js?id=ward0Wz6G6I7UlbSSeZiQ&v=1" type="text/javascript"-->
 		
 		<title>The Twitter Currency Exchange</title>
-	
-		<script type="text/javascript"> 
-			function sendResize()
-			{
-			  var newWidth = document.getElementById('widthInput').value;
-			  var newHeight = document.getElementById('heightInput').value;
-			  document.getElementById('zoop').width = newWidth;
-			  document.getElementById('zoop').height = newHeight;
-			}
-		</script> 
 		
 		<link rel="stylesheet" type="text/css" href="sandbox.css" />
 	</head> 
@@ -64,35 +57,14 @@
 		
 		<div class="spacer"></div>
 
-		<div id='mainFrame'>
-			<p>
-				<applet 
-					archive = 'zoop.jar' 
-					code = 'zoop.Zoop.class' 
-					width = 600 
-					height = 400 
-					id = 'zoop'>  
-				</applet>
-			</p> 
+		<iframe class="display" src="game.php" width="100%" height="480px" frameBorder="0">
+			<p>Your browser does not support iframes.</p>
+		</iframe>
+		
+		
 
-			<p>
-				width: <input id='widthInput' value='600'>  
-				height: <input id='heightInput' value='400'>  
-				<button onClick='sendResize()'> resize </button> 
-			</p>
-		</div>
-
-		<div id='footerFrame'>
-			
-			<p> 
-				footer 
-			</p>
-			
-			<p>
-				<?php 
-				  echo "php debug info";
-				?>
-			</p>
+		<div id='footer'>
+				Copyright (c) 2011  PSU ACM 
 		</div>
 
 	</body> 
